f4ba85d73b4f86d52119a8793e41fd02faf76735
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2023-02-14 15:33:36 +0000

    fixed bug within urls and views

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -129,3 +128,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Contact email
+
+CONTACT_EMAIL = 'nfareborg@gmail.com'
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
-
+    path('', include('base.urls')),
+    path("contact/", include("contact.urls")),
 ]
