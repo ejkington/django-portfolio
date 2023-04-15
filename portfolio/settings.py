cd37885a3242b578a3eab415c4eba37c38737927
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2023-04-15 18:55:55 +0000

    cloudinary settings up

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -12,17 +12,19 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+if os.path.isfile("env.py"):
+    import env
 
+SECRET_KEY = os.environ.get('21q36w4rhgedrtu347yehd5732wetsfgdrxr567w53wer')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f*f_u9re!#2=inswrn&qttun7@%2xsemk2wcgx1vpjarsn^2ed'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
+
+
 
 ]
 
@@ -77,10 +84,8 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':
+        dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
@@ -119,7 +124,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+STATICFILES_STORAGE ='cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+
+DEFAULT_FILE_STORAGE ='cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
