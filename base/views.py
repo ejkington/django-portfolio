7416a85006f84f808a244a9faf5b5122c37b7ba3
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2023-04-16 09:47:06 +0000

    mime types bugg fix.

--- base/views.py
+++ base/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
     return render(request, 'base/home.html')
+
+
+def my_view(request):
+    response = HttpResponse(open('/path/to/your/css/file.css', 'r').read())
+    response['Content-Type'] = 'text/css'
+    return response
