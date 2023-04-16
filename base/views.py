da988ebd299bcce3bcddb1c3ab08a9b6427b0b78
Niklas Tommy Färeborg <Fareborg@hotmail.com>
2023-04-16 10:08:23 +0000

    mimetypes fixing

--- base/views.py
+++ base/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'base/home.html')
+
+
+def my_view(request):
+    response = HttpResponse(open('/static/css/main.css', 'r').read())
+    response['Content-Type'] = 'text/css'
+    return response
